2e4d208e43d8736db36c040e9c2e752ce2c01066
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-28 17:59:48 -0300

    Added setup for eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'prettier',
+    'prettier/react',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'prettier',
+    'react',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.js']}],
+    'import/prefer-default-export': 'off',
+  },
+};
