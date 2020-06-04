4f919abc9d7ce2bcb3424d50db9565988adf3263
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-06-04 19:28:03 -0300

    Agora carrinho no header utiliza reducer de cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { MdShoppingCart } from 'react-icons/md';
 
@@ -7,7 +8,7 @@ import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -17,7 +18,9 @@ function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span> 
+{' '}
+{cartSize} itens </span>
         </div>
         <MdShoppingCart size={36} color="#FFF" />
       </Cart>
@@ -25,4 +28,6 @@ function Header() {
   );
 }
 
-export default Header;
+export default connect((state) => ({
+  cartSize: state.cart.length,
+}))(Header);
